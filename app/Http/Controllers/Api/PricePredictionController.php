e8e7a3d0bd080ecedc7f5d5efd909dd1ae1f7ee2
Fatih Sarikaya <fatihsarikaya4@hotmail.com>
2023-01-18 11:01:28 +0100

    Add files via upload

--- app/Http/Controllers/Api/PricePredictionController.php
+++ app/Http/Controllers/Api/PricePredictionController.php
@@ -6,6 +6,10 @@ use App\Core\Controller;
 use App\Core\HttpResponse;
 use App\Http\Requests\Api\PricePredictionController\CheckRequest;
 use App\Interfaces\PricePrediction\IPricePredictionService;
+use Illuminate\Support\Facades\Log;
+use Monolog\Handler\FirePHPHandler;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 
 class PricePredictionController extends Controller
 {
@@ -40,6 +44,14 @@ class PricePredictionController extends Controller
             $request->bodyType,
             $request->doors
         );
+        Log::channel('laravel.log')->info(json_encode($request->all()));
+        Log::channel('laravel.log')->info(json_encode($response->getData()));
+
+        //$logger = new Logger('my_logger');
+        //$logger->pushHandler(new StreamHandler(__DIR__.'../../../../../resources/log/vin2.log',5,Logger::DEBUG,true,false ));
+        //$logger->pushHandler(new FirePHPHandler());
+        //$logger->info(json_encode($request->all()));
+        //$logger->info(json_encode($response->getData()));
 
         return $this->httpResponse(
             $response->getMessage(),
